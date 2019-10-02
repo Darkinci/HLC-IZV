e772007f2e6131dc1e771ac498eb2b44d2e09b8c
Darkinci <jorge.ortega.nieto@gmail.com>
2019-10-02 10:35:48 +0200

    He añadido otro echo con "hola mundo 2"

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 		<title>Prueba de PHP</title>
 	</head>
 	<body>
-		<?php echo '<h1 style='color:green; font-style:center'>Hola Mundo</h1>'; ?>
+		<?php
+			echo '<h1 style='color:green; font-style:center'>Hola Mundo</h1>'; 
+			echo '<h2> Hola mundo 2</h2>';
+		
+		?>
 	</body>
 </html>
